9095170ca7dd5160d9f860d09b04193a5cf85ad6
diana lopes <dmcls@iscte-iul.pt>
2018-04-28 23:01:57 +0100

    God Class

--- mxgraph-master/java/src/com/mxgraph/io/mxObjectCodec.java
+++ mxgraph-master/java/src/com/mxgraph/io/mxObjectCodec.java
@@ -34,7 +34,9 @@ import com.mxgraph.util.mxUtils;
 public class mxObjectCodec
 {
 
-	private static final Logger log = Logger.getLogger(mxObjectCodec.class.getName());
+	private mxObjectCodecProduct mxObjectCodecProduct = new mxObjectCodecProduct();
+
+	public static final Logger log = Logger.getLogger(mxObjectCodec.class.getName());
 
 	/**
 	 * Immutable empty set.
@@ -66,16 +68,6 @@ public class mxObjectCodec
 	 * Maps from from XML attribute names to fieldnames.
 	 */
 	protected Map<String, String> reverse;
-
-	/**
-	 * Caches accessors for the given method names.
-	 */
-	protected Map<String, Method> accessors;
-
-	/**
-	 * Caches fields for faster access.
-	 */
-	protected Map<Class, Map<String, Field>> fields;
 
 	/**
 	 * Constructs a new codec for the specified template object.
@@ -367,7 +359,7 @@ public class mxObjectCodec
 				if ((f.getModifiers() & Modifier.TRANSIENT) != Modifier.TRANSIENT)
 				{
 					String fieldname = f.getName();
-					Object value = getFieldValue(obj, fieldname);
+					Object value = mxObjectCodecProduct.getFieldValue(obj, fieldname);
 					encodeValue(enc, obj, fieldname, value, node);
 				}
 			}
@@ -448,7 +440,7 @@ public class mxObjectCodec
 				value = tmp;
 			}
 
-			Object defaultValue = getFieldValue(template, fieldname);
+			Object defaultValue = mxObjectCodecProduct.getFieldValue(template, fieldname);
 
 			if (fieldname == null || enc.isEncodeDefaults()
 					|| defaultValue == null || !defaultValue.equals(value))
@@ -559,50 +551,7 @@ public class mxObjectCodec
 	 */
 	protected Object convertValueFromXml(Class<?> type, Object value)
 	{
-		if (value instanceof String)
-		{
-			String tmp = (String) value;
-
-			if (type.equals(boolean.class) || type == Boolean.class)
-			{
-				if (tmp.equals("1") || tmp.equals("0"))
-				{
-					tmp = (tmp.equals("1")) ? "true" : "false";
-				}
-
-				value = Boolean.valueOf(tmp);
-			}
-			else if (type.equals(char.class) || type == Character.class)
-			{
-				value = Character.valueOf(tmp.charAt(0));
-			}
-			else if (type.equals(byte.class) || type == Byte.class)
-			{
-				value = Byte.valueOf(tmp);
-			}
-			else if (type.equals(short.class) || type == Short.class)
-			{
-				value = Short.valueOf(tmp);
-			}
-			else if (type.equals(int.class) || type == Integer.class)
-			{
-				value = Integer.valueOf(tmp);
-			}
-			else if (type.equals(long.class) || type == Long.class)
-			{
-				value = Long.valueOf(tmp);
-			}
-			else if (type.equals(float.class) || type == Float.class)
-			{
-				value = Float.valueOf(tmp);
-			}
-			else if (type.equals(double.class) || type == Double.class)
-			{
-				value = Double.valueOf(tmp);
-			}
-		}
-
-		return value;
+		return mxObjectCodecProduct.convertValueFromXml(type, value);
 	}
 
 	/**
@@ -652,55 +601,7 @@ public class mxObjectCodec
 	 */
 	protected Field getField(Object obj, String fieldname)
 	{
-		Class<?> type = obj.getClass();
-
-		// Creates the fields cache
-		if (fields == null)
-		{
-			fields = new HashMap<Class, Map<String, Field>>();
-		}
-
-		// Creates the fields cache entry for the given type
-		Map<String, Field> map = fields.get(type);
-
-		if (map == null)
-		{
-			map = new HashMap<String, Field>();
-			fields.put(type, map);
-		}
-
-		// Tries to get cached field
-		Field field = map.get(fieldname);
-
-		if (field != null)
-		{
-			return field;
-		}
-
-		while (type != null)
-		{
-			try
-			{
-				field = type.getDeclaredField(fieldname);
-
-				if (field != null)
-				{
-					// Adds field to fields cache
-					map.put(fieldname, field);
-
-					return field;
-				}
-			}
-			catch (Exception e)
-			{
-				log.log(Level.FINEST, "Failed to get field " + fieldname + " in class " + type, e);
-			}
-
-			type = type.getSuperclass();
-		}
-
-		log.severe("Field " + fieldname + " not found in " + obj);
-		return null;
+		return mxObjectCodecProduct.getField(obj, fieldname);
 	}
 
 	/**
@@ -708,68 +609,7 @@ public class mxObjectCodec
 	 */
 	protected Method getAccessor(Object obj, Field field, boolean isGetter)
 	{
-		String name = field.getName();
-		name = name.substring(0, 1).toUpperCase() + name.substring(1);
-
-		if (!isGetter)
-		{
-			name = "set" + name;
-		}
-		else if (boolean.class.isAssignableFrom(field.getType()))
-		{
-			name = "is" + name;
-		}
-		else
-		{
-			name = "get" + name;
-		}
-
-		Method method = (accessors != null) ? accessors.get(name) : null;
-
-		if (method == null)
-		{
-			try
-			{
-				if (isGetter)
-				{
-					method = getMethod(obj, name, null);
-				}
-				else
-				{
-					method = getMethod(obj, name,
-							new Class[] { field.getType() });
-				}
-			}
-			catch (Exception e)
-			{
-				log.log(Level.SEVERE, "Failed to get method " + name + " from " + obj, e);
-			}
-
-			// Adds accessor to cache
-			if (method != null)
-			{
-				if (accessors == null)
-				{
-					accessors = new Hashtable<String, Method>();
-				}
-
-				accessors.put(name, method);
-			}
-		}
-
-		if (method == null)
-		{
-			// this should be considered an error in the scope of this method, but the
-			// calling code already depends on this method failing softly to filter
-			// non-serializable properties, so it gets called for static fields
-			// (mxCell.serialVersionUID), non-transient-but-probably-should-be fields
-			// (mxCell.children, mxCell.edges)
-			// the proper fix is to rewrite the whole thing to use Introspector, like
-			// encodeFields already intends, so for now let's just log at a lower level
-			if (log.isLoggable(Level.FINER))
-				log.finer("Failed to find accessor for " + field + " in " + obj);
-		}
-		return method;
+		return mxObjectCodecProduct.getAccessor(obj, field, isGetter);
 	}
 
 	/**
@@ -777,27 +617,7 @@ public class mxObjectCodec
 	 */
 	protected Method getMethod(Object obj, String methodname, Class[] params)
 	{
-		Class<?> type = obj.getClass();
-
-		while (type != null)
-		{
-			try
-			{
-				Method method = type.getDeclaredMethod(methodname, params);
-
-				if (method != null)
-				{
-					return method;
-				}
-			}
-			catch (Exception e)
-			{
-				log.log(Level.FINEST, "Failed to get method " + methodname + " in class " + type, e);
-			}
-
-			type = type.getSuperclass();
-		}
-		return null;
+		return mxObjectCodecProduct.getMethod(obj, methodname, params);
 	}
 
 	/**
@@ -806,37 +626,7 @@ public class mxObjectCodec
 	 */
 	protected Object getFieldValue(Object obj, String fieldname)
 	{
-		Object value = null;
-
-		if (obj != null && fieldname != null)
-		{
-			Field field = getField(obj, fieldname);
-
-			try
-			{
-				if (field != null)
-				{
-					if (Modifier.isPublic(field.getModifiers()))
-					{
-						value = field.get(obj);
-					}
-					else
-					{
-						value = getFieldValueWithAccessor(obj, field);
-					}
-				}
-			}
-			catch (IllegalAccessException e1)
-			{
-				value = getFieldValueWithAccessor(obj, field);
-			}
-			catch (Exception e)
-			{
-				log.log(Level.SEVERE, "Failed to get value from field " + fieldname + " in " + obj, e);
-			}
-		}
-
-		return value;
+		return mxObjectCodecProduct.getFieldValue(obj, fieldname);
 	}
 
 	/**
@@ -844,26 +634,7 @@ public class mxObjectCodec
 	 */
 	protected Object getFieldValueWithAccessor(Object obj, Field field)
 	{
-		Object value = null;
-
-		if (field != null)
-		{
-			try
-			{
-				Method method = getAccessor(obj, field, true);
-
-				if (method != null)
-				{
-					value = method.invoke(obj, (Object[]) null);
-				}
-			}
-			catch (Exception e)
-			{
-				log.log(Level.SEVERE, "Failed to get value from field " + field + " in " + obj, e);
-			}
-		}
-
-		return value;
+		return mxObjectCodecProduct.getFieldValueWithAccessor(obj, field);
 	}
 
 	/**
@@ -872,39 +643,7 @@ public class mxObjectCodec
 	 */
 	protected void setFieldValue(Object obj, String fieldname, Object value)
 	{
-		Field field = null;
-
-		try
-		{
-			field = getField(obj, fieldname);
-
-			if (field != null)
-			{
-				if (field.getType() == Boolean.class)
-				{
-					value = (value.equals("1") || String.valueOf(value)
-							.equalsIgnoreCase("true")) ? Boolean.TRUE
-							: Boolean.FALSE;
-				}
-
-				if (Modifier.isPublic(field.getModifiers()))
-				{
-					field.set(obj, value);
-				}
-				else
-				{
-					setFieldValueWithAccessor(obj, field, value);
-				}
-			}
-		}
-		catch (IllegalAccessException e1)
-		{
-			setFieldValueWithAccessor(obj, field, value);
-		}
-		catch (Exception e)
-		{
-			log.log(Level.SEVERE, "Failed to set value \"" + value + "\" to field " + fieldname + " in " + obj, e);
-		}
+		mxObjectCodecProduct.setFieldValue(obj, fieldname, value);
 	}
 
 	/**
@@ -913,37 +652,7 @@ public class mxObjectCodec
 	protected void setFieldValueWithAccessor(Object obj, Field field,
 			Object value)
 	{
-		if (field != null)
-		{
-			try
-			{
-				Method method = getAccessor(obj, field, false);
-
-				if (method != null)
-				{
-					Class<?> type = method.getParameterTypes()[0];
-					value = convertValueFromXml(type, value);
-
-					// Converts collection to a typed array before setting
-					if (type.isArray() && value instanceof Collection)
-					{
-						Collection<?> coll = (Collection<?>) value;
-						value = coll.toArray((Object[]) Array.newInstance(
-								type.getComponentType(), coll.size()));
-					}
-
-					method.invoke(obj, new Object[] { value });
-				}
-			}
-			catch (Exception e)
-			{
-				log.log(Level.SEVERE, "setFieldValue: " + e + " on "
-						+ obj.getClass().getSimpleName() + "."
-						+ field.getName() + " ("
-						+ field.getType().getSimpleName() + ") = " + value
-						+ " (" + value.getClass().getSimpleName() + ")", e);
-			}
-		}
+		mxObjectCodecProduct.setFieldValueWithAccessor(obj, field, value);
 	}
 
 	/**
@@ -1113,7 +822,7 @@ public class mxObjectCodec
 
 			if (!isExcluded(obj, fieldname, value, false))
 			{
-				setFieldValue(obj, fieldname, value);
+				mxObjectCodecProduct.setFieldValue(obj, fieldname, value);
 			}
 		}
 	}
@@ -1146,7 +855,7 @@ public class mxObjectCodec
 
 		if (fieldname == null || !isExcluded(obj, fieldname, child, false))
 		{
-			Object template = getFieldTemplate(obj, fieldname, child);
+			Object template = mxObjectCodecProduct.getFieldTemplate(obj, fieldname, child);
 			Object value = null;
 
 			if (child.getNodeName().equals("add"))
@@ -1179,20 +888,7 @@ public class mxObjectCodec
 	 */
 	protected Object getFieldTemplate(Object obj, String fieldname, Node child)
 	{
-		Object template = getFieldValue(obj, fieldname);
-
-		// Arrays are replaced completely
-		if (template != null && template.getClass().isArray())
-		{
-			template = null;
-		}
-		// Collections are cleared
-		else if (template instanceof Collection)
-		{
-			((Collection<?>) template).clear();
-		}
-
-		return template;
+		return mxObjectCodecProduct.getFieldTemplate(obj, fieldname, child);
 	}
 
 	/**
@@ -1214,7 +910,7 @@ public class mxObjectCodec
 			}
 			else if (fieldname != null && fieldname.length() > 0)
 			{
-				setFieldValue(obj, fieldname, value);
+				mxObjectCodecProduct.setFieldValue(obj, fieldname, value);
 			}
 			// Arrays are treated as collections and
 			// converted in setFieldValue
